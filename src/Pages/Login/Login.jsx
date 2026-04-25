10a0789f88a10ab3a80da24d732c06ecbdfcfcdf
Mohammad Aadil <155073650+aadildevexplorer@users.noreply.github.com>
2026-04-25 13:04:59 +0530

    Update comment for login functionality

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -7,8 +7,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const navigate = useNavigate();
 
-  // to
-  // to login
+  // Login user
   const handleLogin = (e) => {
     e.preventDefault();
     const savedUser = JSON.parse(localStorage.getItem("user"));
